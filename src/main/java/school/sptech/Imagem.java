1736a8b0f56038aa91094b3408a10bb1ff1799ba
enricoferrer <enrico.santana@sptech.school>
2024-11-07 17:55:19 -0300

    exercicio concluido

--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,40 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+    private List<Figura> figuras;
+
+    public void adicionar(Figura figura){
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas(){
+        Double somaAreas = 0.0;
+        for(int i = 0; i < figuras.size();i++){
+            somaAreas += figuras.get(i).calcularArea();
+        }
+        return somaAreas;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20(){
+        List<Figura> figurasMaiorQue20 = new ArrayList<>();
+        for(int i = 0; i < figuras.size();i++){
+            if(figuras.get(i).calcularArea() > 20.0){
+                figurasMaiorQue20.add(figuras.get(i));
+            }
+        }
+        return figurasMaiorQue20;
+    }
+
+    public List<Figura> buscarQuadrados(){
+        List<Figura> quadrados = new ArrayList<>();
+        for(int i = 0; i < figuras.size();i++){
+            if(figuras.get(i) instanceof Quadrado){
+                quadrados.add(figuras.get(i));
+            }
+        }
+        return quadrados;
+    }
 }
--- src/main/java/school/sptech/Retangulo.java
+++ src/main/java/school/sptech/Retangulo.java
@@ -1,4 +1,27 @@
 package school.sptech;
 
-public class Retangulo {
+public class Retangulo extends Figura{
+    private Double altura;
+    private Double base;
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double getBase() {
+        return base;
+    }
+
+    public void setBase(Double base) {
+        this.base = base;
+    }
+
+    @Override
+    public Double calcularArea() {
+        return base * altura;
+    }
 }
